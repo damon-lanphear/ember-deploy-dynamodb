78de38e10f16dfe00fb91ce5fe68c2998f4465a1
Alex May <alex.k.may@gmail.com>
2015-12-04 15:38:43 -0800

    start upgrade to support ember deploy 0.5.0
    Upload has been updated to work with new format. Still need to update
    fetchRevisions and activate hooks.

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
 /* jshint node: true */
 'use strict';
 
+var path      = require('path');
+var fs        = require('fs');
+
+var denodeify = require('rsvp').denodeify;
+var readFile  = denodeify(fs.readFile);
+
+var DeployPluginBase = require('ember-cli-deploy-plugin');
 var DynamoDBAdapter = require('./lib/dynamodb');
 
-module.exports = {
-  name: 'ember-deploy-dynamodb',
-  type: 'ember-deploy-addon',
+var DEFAULT_MANIFEST_SIZE = 10;
 
-  adapters: {
-    index: {
-      'dynamodb': DynamoDBAdapter
-    }
+module.exports = {
+  name: 'ember-cli-deploy-dynamodb',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+
+      defaultConfig: {
+        distDir: function(context) {
+          return context.distDir;
+        },
+        manifestSize: DEFAULT_MANIFEST_SIZE,
+        revisionKey: function(context) {
+          return context.commandOptions.revision || (context.revisionData && context.revisionData.revisionKey);
+        },
+      },
+
+      requiredConfig: ['accessKeyId', 'secretAccessKey', 'region', 'table', 'indexName'],
+
+      upload: function(context) {
+        var revisionKey = this.readConfig('revisionKey');
+        var distDir = this.readConfig('distDir');
+
+        var options = {
+          manifest: this.readConfig('manifestSize'),
+          accessKeyId: this.readConfig('accessKeyId'),
+          secretAccessKey: this.readConfig('secretAccessKey'),
+          region: this.readConfig('region'),
+          table: this.readConfig('table'),
+          indexName: this.readConfig('indexName')
+        };
+
+        var DynamoDB = new DynamoDBAdapter(options);
+
+        return this._readFileContents(path.join(distDir, "index.html"))
+          .then(function(indexContents) {
+            return DynamoDB.upload(indexContents, revisionKey);
+          }).then(this._uploadSuccessMessage.bind(this))
+          .then(function(key) {
+           return { dynamodbKey: key };
+         })
+         .catch(this._errorMessage.bind(this));
+      },
+
+      _readFileContents: function(path) {
+        return readFile(path)
+        .then(function(buffer) {
+          return buffer.toString();
+        });
+      },
+
+      _uploadSuccessMessage: function(key) {
+        this.log('Uploaded with key `' + key + '`', { verbose: true });
+        return Promise.resolve(key);
+      },
+
+      _errorMessage: function(error) {
+        this.log(error, { color: 'red' });
+        return Promise.reject(error);
+      }
+    });
+
+    return new DeployPlugin();
   }
 };
